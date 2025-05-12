f02829d2f67053354f6d5a0690640fcb4fcd8c40
MarieAmbrazhei <marykapuza@gmail.com>
2025-05-13 00:42:57 +0300

    added String Reversal Algorithm

---
+++ algorithm/reverse_string.py
@@ -0,0 +1,41 @@
+# Напишите функцию, которая переворачивает строку, то есть возвращает строку, где символы расположены в обратном порядке.
+# Входные данные:
+# Одна строка s.
+# Выходные данные:
+# Строка, содержащая символы исходной строки в обратном порядке.
+# Пример:
+# Вход: "hello"
+# Выход: "olleh"
+# Ограничения:
+# Длина строки
+# 1≤∣s∣≤10
+# Строка состоит только из печатных символов (букв, цифр, знаков препинания и пробелов).
+
+
+def reverse_string_manual(s):
+    # reversed_str = " "
+    # for char in range(len(s) - 1, -1, -1):
+    #    reversed_str += s[char]
+    # print(reversed_str)
+
+    # reversed_str = s[::-1]
+    # print(reversed_str)
+
+    # reversed_str = ''.join(reversed(s))
+    # print(reversed_str)
+
+    # reversed_str = ""
+    # i = len(s) - 1
+    # while i >= 0:
+    #     reversed_str += s[i]
+    #     i = i - 1
+    # return reversed_str
+
+    reversed_str = ""
+    for char in s:
+        reversed_str = char + reversed_str
+    return reversed_str
+
+
+
+print(reverse_string_manual('Hello'))
